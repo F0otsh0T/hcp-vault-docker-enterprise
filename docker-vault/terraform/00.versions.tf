e26129d72f3130b8a11561267d6e851a9255cd44
F0otsh0T <jychen1@hotmail.com>
2024-02-19 16:00:33 -0800

    [F0otsh0T 20240219] Hello

---
+++ docker-vault/terraform/00.versions.tf
@@ -0,0 +1,52 @@
+# 00.versions.tf
+
+terraform {
+  required_version = ">= 0.13"
+
+  required_providers {
+    local = {
+      source  = "hashicorp/local"
+      version = ">=2.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = ">=3.1.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = ">=3.1.0"
+    }
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "~> 2.16.0"
+    }
+    # vault = {
+    #   source  = "hashicorp/vault"
+    #   version = ">= 3.0.0"
+    # }
+    # keycloak = {
+    #   source  = "mrparkers/keycloak"
+    #   version = ">= 3.0.0"
+    # }
+  }
+}
+
+# locals {
+#   vault_root_token  = var.vault_root_token
+#   keycloak_user     = var.keycloak_user
+#   keycloak_password = var.keycloak_password
+# }
+
+# provider "vault" {
+# #  address = "http://localhost:8200"
+#   address = var.vault_url
+#   token   = local.vault_root_token
+# }
+
+# provider "keycloak" {
+#   client_id = "admin-cli"
+#   username  = local.keycloak_user
+#   password  = local.keycloak_password
+#   base_path = ""
+#   url       = "http://localhost:8080"
+# }
---
+++ docker-vault/terraform/99.outputs.tf
@@ -0,0 +1,84 @@
+# 99.outputs.tf
+
+# output "" {
+#     value = 
+# }
+
+################################################
+# DOCKER IMAGE - VAULT
+################################################
+
+output "container_image_vault_id" {
+    value = docker_image.vault-ent.id
+}
+
+################################################
+# NETWORK
+################################################
+
+output "network_name" {
+    value = docker_network.vault-ent-network.name
+}
+
+output "network_id" {
+    value = docker_network.vault-ent-network.id
+}
+
+output "network_ipam" {
+    value = docker_network.vault-ent-network.ipam_config
+}
+
+################################################
+# VAULT INSTANCES
+################################################
+
+output "container_name_vault_s1" {
+    value = docker_container.vault_s1.name
+}
+
+output "container_ip_vault_s1" {
+    value = docker_container.vault_s1.network_data.0.ip_address
+}
+
+output "container_port_internal_vault_s1" {
+    value = docker_container.vault_s1.ports.0.internal
+}
+
+output "container_port_external_vault_s1" {
+    value = docker_container.vault_s1.ports.0.external
+}
+
+
+output "container_name_vault_s2" {
+    value = docker_container.vault_s2.name
+}
+
+output "container_ip_vault_s2" {
+    value = docker_container.vault_s2.network_data.0.ip_address
+}
+
+output "container_port_internal_vault_s2" {
+    value = docker_container.vault_s2.ports.0.internal
+}
+
+output "container_port_external_vault_s2" {
+    value = docker_container.vault_s2.ports.0.external
+}
+
+output "container_name_vault_s3" {
+    value = docker_container.vault_s3.name
+}
+
+output "container_ip_vault_s3" {
+    value = docker_container.vault_s3.network_data.0.ip_address
+}
+
+output "container_port_internal_vault_s3" {
+    value = docker_container.vault_s3.ports.0.internal
+}
+
+output "container_port_external_vault_s3" {
+    value = docker_container.vault_s3.ports.0.external
+}
+
+
